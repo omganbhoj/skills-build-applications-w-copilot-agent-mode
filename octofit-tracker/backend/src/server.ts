c1d3a522ab9e67edce7771d8877c8d243077506d
om ganbhoj <38558574+omganbhoj@users.noreply.github.com>
2026-07-16 12:06:19 +0000

    codespac neame issue

--- octofit-tracker/backend/src/server.ts
+++ octofit-tracker/backend/src/server.ts
@@ -5,13 +5,22 @@ import teamsRouter from './routes/teams';
 import activitiesRouter from './routes/activities';
 import leaderboardRouter from './routes/leaderboard';
 import workoutsRouter from './routes/workouts';
-import { getApiMetadata, getBaseUrl, getCodespaceName } from './routes/utils';
+import { getApiMetadata,  getCodespaceName } from './routes/utils';
+
+export function getBaseUrl() {
+  const codespaceName = getCodespaceName();
+  return codespaceName
+    ? `https://${codespaceName}-8000.app.github.dev`
+    : 'http://localhost:8000';
+}
 
 const app = express();
 const port = Number(process.env.PORT) || 8000;
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/octofit_db';
 const CODESPACE_NAME = getCodespaceName();
 const URL = getBaseUrl();
+
+
 
 app.use(express.json());
 
